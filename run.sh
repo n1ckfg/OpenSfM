606f7863c46cd826988302e5ee41ac31ba51e242
n1ckfg <nick@fox-gieg.com>
2020-03-19 00:00:40 -0400

    shell scripts

---
+++ run.sh
@@ -0,0 +1,4 @@
+bin/opensfm_run_all data/berlin
+bin/opensfm undistort data/berlin
+bin/opensfm compute_depthmaps data/berlin
+
---
+++ server.sh
@@ -0,0 +1,3 @@
+python -m http.server
+
+# http://localhost:8000/viewer/reconstruction.html#file=/data/berlin/reconstruction.meshed.json
